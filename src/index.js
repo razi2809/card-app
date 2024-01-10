8649baf2d52b9ec2b5b69b5033c67712044bf714
razi2809 <razi28080@gmail.com>
2024-01-10 23:57:33 +0200

    moving back to ubuntu server for best performance

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { Provider } from "react-redux";
 import store from "./REDUX/bigPie";
 import { getToken } from "./service/tokenservice";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-axios.defaults.baseURL = "https://server-side-card-app.onrender.com";
+axios.defaults.baseURL = "https://www.razicardapp.site";
 axios.interceptors.request.use((config) => {
   const tokenoOBj = getToken();
   if (tokenoOBj) {
